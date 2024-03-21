8f8f1a74e69e27adfe38b03569bfe55ee5e789a4
Nikolas Silva <silvannikolas@gmail.com>
2024-03-20 21:36:22 -0300

    adicionando aritimetica

--- index.js
+++ index.js
@@ -60,6 +60,68 @@ function productFees(fees, total)
 }
 console.log(productFees(10, 200))
 
+let num1 = 1;
+let num2 = 2;
+
+console.log(num1+num2)
+console.log(num1-num2)
+console.log(num1*num2)
+console.log(num1/num2)
+console.log(num1%num2)
+console.log(num1**num2)
+console.log(Math.sqrt(25))
+
+//operadores de atribuição
+
+let num3 = 10;
+console.log(num3 +=20); 
+console.log(num3 -=20);
+console.log(num3 *=20);
+console.log(num3 /=20);
+
+//operadores de comparação
+
+let num4 = 10;
+
+console.log(num4 > 0);
+console.log(num4 < 0);
+console.log(num4 >= 10);
+console.log(num4 <= 10);
+console.log(num4 == 10);
+
+console.log(num4 != 10); //diferente ou igual
+
+//operadores igualdade
+
+let num5 = 1;
+let num6 = '1';
+
+console.log(num5 == num6);
+console.log(num5 === num6); //identificar se o valor e o tipo são iguais
+
+//operadores ternários
+
+let driver = 120
+let speed = driver>120 ? "aboove" : "below"
+
+console.log(speed)
+
+//operadores lógicos com strings
+
+let temIdadeMinima = false;
+let temTituloEleitor = true;
+
+let podeVotar = temIdadeMinima && temTituloEleitor;
+
+console.log(podeVotar);
+
+let podeViajar = !podeVotar; //igual ou inverso
+
+console.log(podeViajar);
+
+
+
+
 
 
 
